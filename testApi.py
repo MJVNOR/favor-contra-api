27fb81f5d8600e99820502ad94f2f5f7c3dd4109
Martin Vega <43889311+MJVNOR@users.noreply.github.com>
2021-09-14 21:56:02 -0700

    cambiando la pagina del test al heroku

--- testApi.py
+++ testApi.py
@@ -1,6 +1,8 @@
 import requests
 
 review = {"texto": "Viva amlo"}
-resp = requests.post("http://localhost:8000/clasification", json=review)
+resp = requests.post(
+    "https://favor-contra-api.herokuapp.com/clasification", json=review
+)
 
 print(resp.content)
